aad1bcb5b80db221080649864af92643172ebfc8
ljenchik <pelagenkolena@gmail.com>
2023-10-30 14:18:01 +0000

    Added query

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
-
+require("dotenv").config();
+const api_key = process.env.API_KEY;
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
+
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=emoji&limit=1&offset=0&rating=g&lang=en`;
+
+async function getImage(query) {
+    const response = await fetch(query);
+    const data = await response.json();
+    console.log(data.data[0].url);
+    return data.data[0].url;
+}
+
+getImage(endpoint);
