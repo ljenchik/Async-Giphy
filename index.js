1bb43a966245548934761f5404484f38f52d1e2b
ljenchik <pelagenkolena@gmail.com>
2023-10-30 14:43:27 +0000

    Added helper function to get array of urls

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const api_key = process.env.API_KEY;
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY);
 
-const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=emoji&limit=25`;
-
-async function getImage(query) {
-    const response = await fetch(query);
+//Gets random data out of 25
+async function getRandomImage(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${query}&limit=25`;
+    const response = await fetch(endpoint);
     const data = await response.json();
     const random = Math.floor(Math.random() * 25 + 1);
-    console.log(data.data[random].images.original.url);
     return data.data[random].images.original.url;
 }
 
-getImage(endpoint);
+// Gets 25 pieces of data
+async function getImages(query) {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${query}&limit=25`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    //console.log(data);
+    return data.data;
+}
+
+// Returns an array of 25 urls from the data
+async function helperFunction() {
+    const fetchedArray = await getImages("dogs");
+    const urls = [];
+    for (const item of fetchedArray) {
+        urls.push(item.images.original.url);
+    }
+    console.log(urls);
+    return urls;
+}
+
+console.log(helperFunction());
