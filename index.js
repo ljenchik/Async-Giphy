452fc4b7176d328514382a113dde433bdebbc144
Jordan McNamara <jordan.mcnamara@gmail.com>
2023-10-31 10:42:46 +0000

    suggested approach

--- index.js
+++ index.js
@@ -5,68 +5,51 @@
 
 //Gets random data out of 25
 async function getRandomImage(query) {
-    try {
-        const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=MgLakLPlYdchzPhxGysbroEOds8DCqvE&q=${query}&limit=25`;
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        const random = Math.floor(Math.random() * 25 + 1);
-        return data.data[random].images.original.url;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=MgLakLPlYdchzPhxGysbroEOds8DCqvE&q=${query}&limit=25`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const random = Math.floor(Math.random() * 25 + 1);
+    return data.data[random].images.original.url;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Gets 25 pieces of data
 async function getImages(query) {
-    try {
-        const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=MgLakLPlYdchzPhxGysbroEOds8DCqvE&q=${query}&limit=25`;
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        return data.data;
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=MgLakLPlYdchzPhxGysbroEOds8DCqvE&q=${query}&limit=25`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data.data;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Returns an array of 25 urls from the data
 async function helperFunction(query) {
-    const fetchedArray = await getImages(query);
-    const urls = [];
-    for (const item of fetchedArray) {
-        urls.push(item.images.original.url);
-    }
-    return urls;
+  const fetchedArray = await getImages(query);
+  const urls = [];
+  for (const item of fetchedArray) {
+    urls.push(item.images.original.url);
+  }
+  return urls;
 }
 
 if (typeof window !== "undefined") {
-    // const button = document.querySelector("button");
-    // button.addEventListener("click", async () => {
-    //     const url = await getRandomImage("dogs");
-    //     document.querySelector(
-    //         "#randomImage"
-    //     ).innerHTML = `<img src=${url} width="400"/>`;
-    // });
+  const query = document.getElementById("query");
+  const images = document.getElementById("images");
+  const buttonForManyImages = document.getElementById("many");
 
-    const query = document.getElementById("query");
-    const images = document.getElementById("images");
-    const buttonForManyImages = document.getElementById("many");
-
-    function empty(element) {
-        element.innerHTML = "";
+  buttonForManyImages.addEventListener("click", async () => {
+    images.innerHTML = "";
+    const urls = await helperFunction(query.value);
+    for (const url of urls) {
+      const img = document.createElement("img");
+      img.src = url;
+      images.appendChild(img);
     }
-
-    query.addEventListener("change", () => {
-        const inputValue = query.value;
-        buttonForManyImages.addEventListener("click", async () => {
-            empty(images);
-            console.log(images);
-            const urls = await helperFunction(inputValue);
-            for (const url of urls) {
-                const img = document.createElement("img");
-                img.src = url;
-                images.appendChild(img);
-            }
-        });
-        query.value = "";
-    });
+  });
 }
